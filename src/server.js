04ab485337885c7c7573a31430319c7228b3c1af
krishnaVerma1302 <17krishnaverma@gmail.com>
2025-12-23 15:37:59 +0530

    4th fix cos url in server

--- src/server.js
+++ src/server.js
@@ -11,8 +11,7 @@ const app = express();
 app.use(cors(
   {
     origin:["http://localhost:5173",
-      "https://neeraj-traders.vercel.app",
-      "neeraj-traders-krishnaverma13s-projects.vercel.app"],
+      "https://neeraj-pickle.vercel.app"],
     credentials: true
   }
 ));
